2d5ec02010c6fe856a67e8a608232b61b8ca608e
chandu <chandrakottamasu@gmail.com>
2021-03-26 21:55:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CardModel from './components/CardModel';
+import images from './components/images';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const data = [
+      {"title": "Boat", "price": 300, "image": images[0]},
+      {"title": "Senheiser", "price": 500, "image": images[1]},
+      {"title": "Realme", "price": 600, "image": images[2]}
+    ];
+    return (
+      <div className="App">
+        <div className="row">
+        {data.map((item,index)=> {
+           return <CardModel key={index} title={item.title} price = {item.price} image = {item.image} />;
+        })}
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
